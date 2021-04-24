7716553856ca0cba207cc5e4605f96443fb8e4db
Marcial Rivera <marcial.rivera@autozone.com>
2021-04-24 08:40:59 -0500

    commit inicial

---
+++ Selenium/src/main/java/test/TestSelenium.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+
+	public static void main(String[] args) {
+		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator; //Setear el la ruta de nuestro chromredriver
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //agregando la propiedad con la ruta del chrome driver
+		WebDriver driver = new ChromeDriver(); //inicializando nuestro chrome driver
+		driver.get("https://www.google.com");
+		
+	}
+
+}
---
+++ Selenium/src/test/java/test/NewTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void Test() {
+		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator; //Setear el la ruta de nuestro chromredriver
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //agregando la propiedad con la ruta del chrome driver
+		WebDriver driver = new ChromeDriver(); //inicializando nuestro chrome driver
+		driver.get("https://www.google.com");
+  }
+}
