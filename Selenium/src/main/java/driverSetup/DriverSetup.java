e6aa55f5dd9551543528fc1bf3bfba41e03392e7
Marcial Rivera <marcial.rivera@autozone.com>
2021-05-31 16:38:41 -0500

    cambios adicionales al metodo setupDriver

--- Selenium/src/main/java/driverSetup/DriverSetup.java
+++ Selenium/src/main/java/driverSetup/DriverSetup.java
@@ -19,25 +19,25 @@ public class DriverSetup {
 		
 	}
 	
-	
 	public static WebDriver setupDriver(String browser) {
 		WebDriver driver = null;
-		String exePath;
-		switch(browser) {
+		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator; //Setear el la ruta donde se guardan los drivers
+		
+		switch (browser) {
 		case "chrome":
-			exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator; //Setear el la ruta de nuestro chromredriver
-			System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //agregando la propiedad con la ruta del chrome driver
-			driver = new ChromeDriver(); //inicializando nuestro chrome driver
+			System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");
+			driver = new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			return driver;
+
 		case "firefox":
-			exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator; //Setear el la ruta de nuestro chromredriver
 			System.setProperty("webdriver.gecko.driver", exePath + "geckodriver.exe");
 			driver = new FirefoxDriver();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			return driver;
 		default:
-			return driver;
+			return null;
 		}
+
 	}
 }
